8c54c6669c7f66ac0643745d9b14e61f259ec94e
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2021-02-15 18:32:15 +0000

    More styling

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template("index.html")
+
+@app.route('/contact')
+def contact():
+  return render_template("contact.html")
+
+@app.route('/submit-query', methods=['POST'])
+def submit_query():
+  email = request.form['email']
+  print(email)
+  return redirect("/")
+
+@app.route('/start-now')
+def start_now():
+  return render_template("start-now.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
