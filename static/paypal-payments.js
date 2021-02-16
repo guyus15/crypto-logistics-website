86d70c1d5234088f6cb59e7a06e998577deac825
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2021-02-16 22:55:47 +0000

    fixed paypal payments

--- static/paypal-payments.js
+++ static/paypal-payments.js
@@ -1,10 +1,29 @@
-const container = document.querySelector('.start-now-container');
 const options = document.querySelector('#items');
-const paypalScripts = document.querySelector('.paypal-scripts');
+const paypalContainer = document.querySelector('#paypal-button-container');
 
-const standardButton = `
-  <script src="https://www.paypal.com/sdk/js?client-id=AVznN8KV3QGXYA88LymkE-BH1du9GRPtvKsAuTncB1hBpHGAfpipKvLkIx2jg4jtmZu4aC25BY_x0vPy&vault=true&intent=subscription" data-sdk-integration-source="button-factory"></script>
-  <script>
+paypal.Buttons({
+  style: {
+      shape: 'pill',
+      color: 'silver',
+      layout: 'vertical',
+      label: 'subscribe'
+  },
+  createSubscription: function(data, actions) {
+    return actions.subscription.create({
+      'plan_id': 'P-84G57859S5359031DMAWB4BQ'
+    });
+  },
+  onApprove: function(data, actions) {
+    alert(data.subscriptionID);
+  }
+}).render('#paypal-button-container');
+
+options.addEventListener('change', () => {
+
+  paypalContainer.innerHTML = "";
+
+  if (options.options[options.selectedIndex].value == "item-1")
+  {
     paypal.Buttons({
         style: {
             shape: 'pill',
@@ -14,69 +33,46 @@ const standardButton = `
         },
         createSubscription: function(data, actions) {
           return actions.subscription.create({
-            'plan_id': 'P-64H356549H398381UMAWCAOA'
+            'plan_id': 'P-84G57859S5359031DMAWB4BQ'
           });
         },
         onApprove: function(data, actions) {
           alert(data.subscriptionID);
         }
     }).render('#paypal-button-container');
-  </script>
-`
-
-const premiumButton = `
-<script src="https://www.paypal.com/sdk/js?client-id=AVznN8KV3QGXYA88LymkE-BH1du9GRPtvKsAuTncB1hBpHGAfpipKvLkIx2jg4jtmZu4aC25BY_x0vPy&vault=true&intent=subscription" data-sdk-integration-source="button-factory"></script>
-<script>
-  paypal.Buttons({
-      style: {
-          shape: 'pill',
-          color: 'silver',
-          layout: 'vertical',
-          label: 'subscribe'
-      },
-      createSubscription: function(data, actions) {
-        return actions.subscription.create({
-          'plan_id': 'P-2WT76139PW990912VMAWB6YQ'
-        });
-      },
-      onApprove: function(data, actions) {
-        alert(data.subscriptionID);
-      }
-  }).render('#paypal-button-container');
-</script>
-`
-
-const fullButton = `
-<script src="https://www.paypal.com/sdk/js?client-id=AVznN8KV3QGXYA88LymkE-BH1du9GRPtvKsAuTncB1hBpHGAfpipKvLkIx2jg4jtmZu4aC25BY_x0vPy&vault=true&intent=subscription" data-sdk-integration-source="button-factory"></script>
-<script>
-  paypal.Buttons({
-      style: {
-          shape: 'pill',
-          color: 'silver',
-          layout: 'vertical',
-          label: 'subscribe'
-      },
-      createSubscription: function(data, actions) {
-        return actions.subscription.create({
-          'plan_id': 'P-84G57859S5359031DMAWB4BQ'
-        });
-      },
-      onApprove: function(data, actions) {
-        alert(data.subscriptionID);
-      }
-  }).render('#paypal-button-container');
-</script>
-`
-
-options.addEventListener('change', () => {
-  paypalScripts.innerHTML = ""
-
-  if (options.options[options.selectedIndex].value == "item-1")
-  {
-    paypalScripts.innerHTML += standardButton;
   } else if (options.options[options.selectedIndex].value == "item-2") {
-    paypalScripts.innerHTML += premiumButton;
+      paypal.Buttons({
+          style: {
+              shape: 'pill',
+              color: 'silver',
+              layout: 'vertical',
+              label: 'subscribe'
+          },
+          createSubscription: function(data, actions) {
+            return actions.subscription.create({
+              'plan_id': 'P-2WT76139PW990912VMAWB6YQ'
+            });
+          },
+          onApprove: function(data, actions) {
+            alert(data.subscriptionID);
+          }
+      }).render('#paypal-button-container');
   } else if (options.options[options.selectedIndex].value == "item-3") {
-    paypalScripts.innerHTML += fullButton;
+      paypal.Buttons({
+          style: {
+              shape: 'pill',
+              color: 'silver',
+              layout: 'vertical',
+              label: 'subscribe'
+          },
+          createSubscription: function(data, actions) {
+            return actions.subscription.create({
+              'plan_id': 'P-64H356549H398381UMAWCAOA'
+            });
+          },
+          onApprove: function(data, actions) {
+            alert(data.subscriptionID);
+          }
+      }).render('#paypal-button-container');
   }
-})
+});
