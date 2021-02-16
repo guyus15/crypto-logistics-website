b260c85d405e0e7ad9e5e612022abea90a703a7e
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2021-02-16 19:20:19 +0000

    Added mailer

--- __init__.py
+++ __init__.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request, redirect
+import smtplib
+from email.message import EmailMessage
+
+email_login = "websitemailer4@gmail.com"
+password = "LogitechTHX"
 
 app = Flask(__name__)
 
@@ -12,8 +17,21 @@ def contact():
 
 @app.route('/submit-query', methods=['POST'])
 def submit_query():
+  name = request.form['name']
   email = request.form['email']
-  print(email)
+  subject = request.form['subject']
+  query = request.form['query']
+
+  msg = EmailMessage()
+  msg['Subject'] = subject
+  msg['From'] = email_login
+  msg['To'] = 'crypto-logistics@mail.com'
+  msg.set_content("Query from " + name + "\n\n" + query)
+
+  with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(email_login, password)
+    smtp.send_message(msg)
+ 
   return redirect("/")
 
 @app.route('/start-now')
